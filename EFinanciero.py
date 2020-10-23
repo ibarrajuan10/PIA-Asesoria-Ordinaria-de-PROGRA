d3e39f2431c9c444aa4e09188974d85eb92bbac7
ibarrajuan10 <juan.ibarra.mty@outlook.com>
2020-10-22 21:51:25 -0500

    Agregue la funcion RealizarEF

--- EFinanciero.py
+++ EFinanciero.py
@@ -11,3 +11,16 @@ class EstadoFinanciero:
         self.__GIDFF=GIDFF
         self.__GCMPDF=GCMPDF
         self.__DRCMPDF=DRCMPDF
+    
+    def RealizarEF(self):
+        ComprasTotales=(self.__CMPDF+self.__GCMPDF)
+        ComprasNetasDeMateriales=(ComprasTotales-self.__DRCMPDF)
+        MaterialesDisponibles=(ComprasNetasDeMateriales+self.__MPDI)
+        MateriaPrimaUtilizada=(MaterialesDisponibles-self.__MPDF)
+        CostoPrimo=(MateriaPrimaUtilizada+self.__MODF)
+        CostoIncurrido=(CostoPrimo+self.__GIDFF)
+        TotalDeProduccionEnProcesos=(CostoIncurrido+self.__PPI)
+        CostoDeProduccion=(TotalDeProduccionEnProcesos-self.__PPF)
+        TotalDeArticulosListosParaLaVenta=(CostoDeProduccion+self.__ATI)
+        CostoDeProduccionDeLoVendido=(TotalDeArticulosListosParaLaVenta-self.__ATF)
+        
