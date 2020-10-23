5d1ac66e71d0347597f582a1cf5ae0d8774ef6bc
ibarrajuan10 <juan.ibarra.mty@outlook.com>
2020-10-22 21:48:38 -0500

    Agregue la clase

---
+++ EFinanciero.py
@@ -0,0 +1,13 @@
+class EstadoFinanciero:
+    def __init__(self,MPDI,PPI,ATI,MPDF,PPF,ATF,MODF,CMPDF,GIDFF,GCMPDF,DRCMPDF):
+        self.__MPDI=MPDI
+        self.__PPI=PPI
+        self.__ATI=ATI
+        self.__MPDF=MPDF
+        self.__PPF=PPF
+        self.__ATF=ATF
+        self.__MODF=MODF
+        self.__CMPDF=CMPDF
+        self.__GIDFF=GIDFF
+        self.__GCMPDF=GCMPDF
+        self.__DRCMPDF=DRCMPDF
